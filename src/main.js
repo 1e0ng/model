ac58c0b3f4c3b9e307e22ad9c0c2aa00efe5b428
Leon <i@leons.im>
2016-05-31 21:47:11 +0800

    Add author.

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
 d3.select('#slider4').call(d3.slider().axis(true).min(100).max(3000).on("slide", function(evt, value) {
   d3.select('#slider4text').text(Math.round(value * 100) / 100);
 }).value(300));
+
+
