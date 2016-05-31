de44cfc66dc8be9e095b46eb3597df79fbe6800b
Leon <i@leons.im>
2016-05-31 23:00:04 +0800

    Refactor gulpfile and add bootstrap.

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,47 @@
 var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
+    autoprefixer = require('gulp-autoprefixer'),
     rename = require('gulp-rename'),
     concat = require('gulp-concat'),
     notify = require('gulp-notify'),
     less = require('gulp-less'),
-    minify = require('gulp-minify-css');
+    minify = require('gulp-minify-css'),
+    livereload = require('gulp-livereload'),
+    del = require('del');
+
+gulp.task('styles', function() {
+  return gulp.src('src/*.less')
+    .pipe(less())
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+    .pipe(concat('main.css'))
+    .pipe(minify())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('static'));
+});
+
+gulp.task('scripts', function() {
+  return gulp.src('src/*.js')
+    .pipe(concat('main.js'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('static'));
+});
+
+gulp.task('clean', function(cb) {
+  del(['static'], cb);
+});
 
 gulp.task('default', function() {
-  gulp.src('src/*.js')
-                 .pipe(concat('main.js'))
-                 .pipe(rename({suffix: '.min'}))
-                 .pipe(uglify())
-                 .pipe(gulp.dest('static'))
+  gulp.start('styles', 'scripts');
+});
 
-  gulp.src('src/*.less')
-                 .pipe(less())
-                 .pipe(concat('main.css'))
-                 .pipe(minify())
-                 .pipe(rename({suffix: '.min'}))
-                 .pipe(gulp.dest('static'))
+gulp.task('watch', function() {
+  // Watch .scss files
+  gulp.watch('src/*.less', ['styles']);
+  // Watch .js files
+  gulp.watch('src/*.js', ['scripts']);
+  // Create LiveReload server
+  livereload.listen();
+  // Watch any files in dist/, reload on change
+  gulp.watch(['static/**']).on('change', livereload.changed);
 });
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
-d3.select('#slider4').call(d3.slider().axis(true).min(100).max(3000).on("slide", function(evt, value) {
-  d3.select('#slider4text').text(Math.round(value * 100) / 100);
-}).value(300));
+d3.select('#construct-area').call(d3.slider().axis(d3.svg.axis().ticks(10)).min(200).max(2000).step(50).on("slide", function(evt, value) {
+  d3.select('#construct-area-value').text(value);
+}));
+d3.select('#construct-area').call(d3.sider().value(300));
 
 
