04728a5e0e7c4d422f641117088fd0a14073d3a5
Leon <i@leons.im>
2016-05-31 23:29:11 +0800

    Update page layout.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
-d3.select('#construct-area').call(d3.slider().axis(d3.svg.axis().ticks(10)).min(200).max(2000).step(50).on("slide", function(evt, value) {
-  d3.select('#construct-area-value').text(value);
-}));
-d3.select('#construct-area').call(d3.sider().value(300));
+var areaSlider = d3.slider()
+  .axis(d3.svg.axis().ticks(10))
+  .min(200)
+  .max(2000)
+  .step(50)
+  .on("slide", function(evt, value) {
+    d3.select('#area-text').text(value);
+  });
+d3.select('#area').call(areaSlider);
+areaSlider.value(300);
 
 
