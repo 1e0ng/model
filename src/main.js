5ed19bf46b0e161cfa7e78dae2a08219eeb1a791
Leon <i@leons.im>
2016-06-01 00:26:02 +0800

    Add property costs.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 function refresh() {
   var area = areaSlider.value();
   var rentPerSquare = rentPerSquareSlider.value();
+  var propertyCostsPerSquare = propertyCostsPerSquareSlider.value();
+  propertyCostsPerSquare = Math.round(propertyCostsPerSquare * 10) / 10;
   var rentFirstYear = area * rentPerSquare * 12;
   d3.select('#area-text').text(area);
   d3.select('#rent-per-square-text').text(rentPerSquare);
   d3.select('#rent-first-year-text').text(rentFirstYear);
+  d3.select('#property-costs-per-square-text').text(propertyCostsPerSquare);
 }
 var areaSlider = d3.slider()
   .axis(d3.svg.axis().ticks(10))
@@ -27,6 +30,17 @@ var rentPerSquareSlider = d3.slider()
     refresh();
   });
 d3.select('#rent-per-square').call(rentPerSquareSlider);
+
+var propertyCostsPerSquareSlider = d3.slider()
+  .axis(d3.svg.axis().ticks(11))
+  .min(0)
+  .max(20)
+  .value(2)
+  .step(0.1)
+  .on('slide', function(evt, value) {
+    refresh();
+  });
+d3.select('#property-costs-per-square').call(propertyCostsPerSquareSlider);
 
 refresh();
 
