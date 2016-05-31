bd9791b81d991e2b46115e2e6c2f562aaeca3aaa
Leon <i@leons.im>
2016-06-01 00:11:57 +0800

    Add rent price.

--- src/main.js
+++ src/main.js
@@ -1,12 +1,32 @@
+function refresh() {
+  var area = areaSlider.value();
+  var rentPerSquare = rentPerSquareSlider.value();
+  var rentFirstYear = area * rentPerSquare * 12;
+  d3.select('#area-text').text(area);
+  d3.select('#rent-per-square-text').text(rentPerSquare);
+  d3.select('#rent-first-year-text').text(rentFirstYear);
+}
 var areaSlider = d3.slider()
   .axis(d3.svg.axis().ticks(10))
   .min(200)
   .max(2000)
+  .value(300)
   .step(50)
   .on("slide", function(evt, value) {
-    d3.select('#area-text').text(value);
+    refresh();
   });
 d3.select('#area').call(areaSlider);
-areaSlider.value(300);
 
+var rentPerSquareSlider = d3.slider()
+  .axis(d3.svg.axis().ticks(7))
+  .min(0)
+  .max(300)
+  .value(40)
+  .step(1)
+  .on('slide', function(evt, value) {
+    refresh();
+  });
+d3.select('#rent-per-square').call(rentPerSquareSlider);
+
+refresh();
 
