b2b05301c260a1f63e02ef5173660d044438a0a2
Leon <i@leons.im>
2016-06-02 18:51:03 +0800

    Update table 1 part 2.

--- src/main.js
+++ src/main.js
@@ -43,11 +43,12 @@ function g(name, ticks, min, max, value, step) {
   d3.select(/* @mangle */'.'/* @/mangle */ + name + /* @mangle */' .graph'/* @/mangle */).call(s[name]);
 }
 function o(k, v) {
+  v = Math.round(v * 100) / 100;
   d3.selectAll(/* @mangle */'.'/* @/mangle */ + k + /* @mangle */'.text'/* @/mangle */).text(v);
 }
 
 function w(k) {
-  return parseInt(d3.select('.' + k + '.text').text());
+  return Math.round(parseFloat(d3.select('.' + k + '.text').text()) * 100) / 100;
 }
 
 function ss() {
@@ -64,26 +65,54 @@ function t() {
   o('start-fund', s['start-fund-in-10k'].value() * 10000);
   o('rent-deposite', s['rent-deposite-in-k'].value() * 1000);
   o('rent-first-year', s['area'].value() * s['rent-per-square'].value() * s['op-month-first-year'].value());
-  o('property-costs-first-year', s['area'].value() * s['op-month-first-year'].value() * s['property-costs-per-square'].value());
-  o('furniture-costs', s['area'].value() * s['furniture-costs-per-square'].value());
-  o('device-costs', s['device-price'].value() * s['classrooms'].value());
-  o('desk-costs', s['desks'].value() * s['desk-price'].value());
+  o('property-cost-first-year', s['area'].value() * s['op-month-first-year'].value() * s['property-cost-per-square'].value());
+  o('furniture-cost', s['area'].value() * s['furniture-cost-per-square'].value());
+  o('device-cost', s['device-price'].value() * s['classrooms'].value());
+  o('desk-cost', s['desks'].value() * s['desk-price'].value());
   var v = s['salary'].value() * s['hr-first-year'].value() * s['op-month-first-year'].value();
-  o('salary-costs', v);
-  o('social-security-costs', v * s['social-security'].value() / 100);
-  o('cpf-costs', v * s['cpf'].value() / 100);
+  o('salary-cost', v);
+  o('social-security-cost', v * s['social-security'].value() / 100);
+  o('cpf-cost', v * s['cpf'].value() / 100);
 
   o('initial-fee', s['initial-fee-in-10k'].value() * 10000);
-  o('first-year-costs', ss('rent-deposite', 'rent-first-year', 'property-costs-first-year', 'furniture-costs', 'device-costs', 'desk-costs', 'salary-costs', 'social-security-costs', 'cpf-costs'));
-  o('first-year-reminder', w('start-fund') - w('first-year-costs'));
+  o('constant-cost-first-year', ss('rent-deposite', 'rent-first-year', 'property-cost-first-year', 'furniture-cost', 'device-cost', 'desk-cost', 'salary-cost', 'social-security-cost', 'cpf-cost'));
+  o('first-year-constant-reminder', w('start-fund') - w('constant-cost-first-year'));
+
+  o('p2p-income-first-year', s['p2p-students'].value() * s['p2p-avg-charge'].value());
+  o('sm-income-first-year', s['sm-students'].value() * s['sm-avg-charge'].value());
+  o('bg-income-first-year', s['bg-students'].value() * s['bg-avg-charge'].value());
+
+  o('total-income-first-year', ss('p2p-income-first-year', 'sm-income-first-year', 'bg-income-first-year'));
+
+  o('support-cost-first-year', w('total-income-first-year') * s['support-commission'].value() / 100);
+
+  v = (w('p2p-income-first-year') * s['p2p-avg-complete-rate'].value() + w('sm-income-first-year') * s['sm-avg-complete-rate'].value() + w('bg-income-first-year') * s['bg-avg-complete-rate'].value()) / 100;
+  o('vat-first-year', v * s['vat'].value() / 100);
+  o('local-tax-first-year', w('vat-first-year') * s['local-tax'].value() / 100);
+  o('sales-commission-first-year', w('total-income-first-year') * s['sales-commission'].value() / 100);
+
+  o('pay-p2p-teacher-first-year', w('p2p-income-first-year') * s['p2p-avg-complete-rate'].value() * s['pay-teacher-percent-for-p2p'].value() / 10000);
+  o('pay-class-teacher-first-year', (w('sm-income-first-year') * s['sm-avg-complete-rate'].value() + w('bg-income-first-year') * s['bg-avg-complete-rate'].value()) * s['pay-teacher-percent-for-class'].value() / 10000);
+  o('market-cost-first-year', v * s['market-cost-percent'].value() / 100);
+
+  o('tutoring-cost-first-year', 0);
+  o('water-elec-cost-first-year', 0);
+
+  o('office-material-cost-first-year', s['hr-first-year'].value() * s['op-month-first-year'].value() * s['office-material-cost-per-person'].value());
+  o('finance-cost-first-year', w('total-income-first-year') * s['credit-card-fee-percent'].value() * s['credit-card-contrib'].value() / 10000);
+
+  o('var-cost-first-year', ss('support-cost-first-year', 'vat-first-year', 'local-tax-first-year', 'sales-commission-first-year', 'pay-p2p-teacher-first-year', 'pay-class-teacher-first-year', 'market-cost-first-year', 'tutoring-cost-first-year', 'water-elec-cost-first-year', 'office-material-cost-first-year', 'finance-cost-first-year'));
+
+  o('first-year-var-reminder', w('total-income-first-year') - w('var-cost-first-year'));
+  o('first-year-reminder', w('first-year-var-reminder') + w('first-year-constant-reminder'));
 }
 g(/* @mangle */'area'/* @/mangle */, 10, 200, 2000, 300, 50);
 g(/* @mangle */'rent-per-square'/* @/mangle */, 7, 0, 300, 40, 1);
 g(/* @mangle */'rent-months'/* @/mangle */, 10, 0, 120, 36, 6);
-g(/* @mangle */'property-costs-per-square'/* @/mangle */, 11, 0, 20, 2, 1);
+g(/* @mangle */'property-cost-per-square'/* @/mangle */, 11, 0, 20, 2, 1);
 g(/* @mangle */'op-month-first-year'/* @/mangle */, 13, 0, 12, 6, 1);
 g(/* @mangle */'classrooms'/* @/mangle */, 13, 0, 100, 18, 1);
-g(/* @mangle */'furniture-costs-per-square'/* @/mangle */, 11, 0, 2000, 700, 10);
+g(/* @mangle */'furniture-cost-per-square'/* @/mangle */, 11, 0, 2000, 700, 10);
 g(/* @mangle */'hr-first-year'/* @/mangle */, 11, 0, 100, 10, 1);
 g(/* @mangle */'salary'/* @/mangle */, 11, 1000, 5000, 2000, 100);
 g(/* @mangle */'desks'/* @/mangle */, 11, 0, 200, 30, 1);
@@ -104,20 +133,20 @@ g(/* @mangle */'bg-avg-complete-rate'/* @/mangle */, 10, 0, 100, 95, 5);
 g(/* @mangle */'sales-commission'/* @/mangle */, 10, 0, 50, 5, 1);
 g(/* @mangle */'course-material-cost'/* @/mangle */, 10, 0, 100, 20, 5);
 
-g(/* @mangle */'p2p-income-contrib'/* @/mangle */, 10, 0, 100, 40, 1);
-g(/* @mangle */'class-income-contrib'/* @/mangle */, 10, 0, 100, 30, 1);
+g(/* @mangle */'pay-teacher-percent-for-p2p'/* @/mangle */, 10, 0, 100, 40, 1);
+g(/* @mangle */'pay-teacher-percent-for-class'/* @/mangle */, 10, 0, 100, 30, 1);
 
 g(/* @mangle */'vat'/* @/mangle */, 3, 0, 6, 3, 3);
 g(/* @mangle */'local-tax'/* @/mangle */, 10, 0, 30, 12, 1);
 
-g(/* @mangle */'credit-card-fees'/* @/mangle */, 10, 0, 3, 0.78, 0.01);
+g(/* @mangle */'credit-card-fee-percent'/* @/mangle */, 10, 0, 3, 0.78, 0.01);
 g(/* @mangle */'credit-card-contrib'/* @/mangle */, 10, 0, 100, 70, 1);
-g(/* @mangle */'support-costs'/* @/mangle */, 10, 0, 50, 5, 1);
+g(/* @mangle */'support-commission'/* @/mangle */, 10, 0, 50, 5, 1);
 
 g(/* @mangle */'social-security'/* @/mangle */, 10, 0, 30, 8, 1);
 g(/* @mangle */'cpf'/* @/mangle */, 10, 0, 20, 4, 1);
 
-g(/* @mangle */'office-material-costs'/* @/mangle */, 10, 0, 200, 50, 10);
+g(/* @mangle */'office-material-cost-per-person'/* @/mangle */, 10, 0, 200, 50, 10);
 
 g(/* @mangle */'percent-for-teaching'/* @/mangle */, 10, 0, 100, 80, 1);
 g(/* @mangle */'percent-for-office'/* @/mangle */, 10, 0, 100, 20, 1);
